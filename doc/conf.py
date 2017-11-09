dfdc671b769dbc9154f8d9719d7ba0468fdf70cc
Jeremy Barlow <jeremy_barlow@mcafee.com>
2017-11-09 11:34:19 -0800

    Fixed copyright in doc conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = "Elasticsearch DXL Python Service"
-copyright = "Copyright 2017"
+copyright = "2017, McAfee LLC"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
